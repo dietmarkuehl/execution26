e3fdb77ee36cc0c5754123118d5bb627f6a51004
dietmarkuehl <dietmar.kuehl@gmail.com>
2025-05-04 21:22:27 +0000

    Deploying to gh-pages from @ dietmarkuehl/execution26@836c06b7e69944c899f4e89397fb328309514bdd 🚀

--- search/all_8.js
+++ search/all_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['never_5fstop_5ftoken_0',['never_stop_token',['../classbeman_1_1execution26_1_1never__stop__token.html',1,'beman::execution26']]],
-  ['nostopstate_5ft_1',['nostopstate_t',['../structbeman_1_1execution26_1_1nostopstate__t.html',1,'beman::execution26']]]
+  ['just_0',['just',['../namespacebeman_1_1execution.html#acdbad94bfa3d61a849a61c99a753d5da',1,'beman::execution']]],
+  ['just_5ferror_1',['just_error',['../namespacebeman_1_1execution.html#a4043903f88f6bb02acdf1c1c369f4ead',1,'beman::execution']]],
+  ['just_5fstopped_2',['just_stopped',['../namespacebeman_1_1execution.html#a6406f8f62cb941fe811fedeebf0b985f',1,'beman::execution']]]
 ];
--- search/variables_2.js
+++ search/variables_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['set_5ferror_0',['set_error',['../namespacebeman_1_1execution26.html#a5a55250379dfed82487b42003ff270b7',1,'beman::execution26']]],
-  ['set_5fstopped_1',['set_stopped',['../namespacebeman_1_1execution26.html#ab5950df93e9bed15bda2d87615fe6f39',1,'beman::execution26']]],
-  ['set_5fvalue_2',['set_value',['../namespacebeman_1_1execution26.html#ae49d099ae6a14cda99cd4b5a7f0be069',1,'beman::execution26']]]
+  ['just_0',['just',['../namespacebeman_1_1execution.html#acdbad94bfa3d61a849a61c99a753d5da',1,'beman::execution']]],
+  ['just_5ferror_1',['just_error',['../namespacebeman_1_1execution.html#a4043903f88f6bb02acdf1c1c369f4ead',1,'beman::execution']]],
+  ['just_5fstopped_2',['just_stopped',['../namespacebeman_1_1execution.html#a6406f8f62cb941fe811fedeebf0b985f',1,'beman::execution']]]
 ];
